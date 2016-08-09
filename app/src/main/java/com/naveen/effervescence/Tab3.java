655c95dbabd1892b6e67025dd45902e6afa0c9b5
Naveen <shuklank2709@gmail.com>
2016-08-09 05:53:59 +0530

    customized buttons

--- app/src/main/java/com/naveen/effervescence/Tab3.java
+++ app/src/main/java/com/naveen/effervescence/Tab3.java
@@ -134,17 +134,19 @@ public class Tab3 extends AppCompatActivity  implements NavigationView.OnNavigat
     public boolean onNavigationItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.category) {
 
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.day) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.proshows) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.proshows) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.bioscope) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.sponsers) {
+
+        }else if (id == R.id.organizers) {
 
         }
 
