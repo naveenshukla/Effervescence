0363a1b116435fc66d38e1c250d638a5c79ecbae
naveen shukla <shuklank2709@gmail.com>
2016-10-08 17:02:50 +0530

    bugs

--- app/src/main/java/com/naveen/effervescence/Utils/OrganizersList.java
+++ app/src/main/java/com/naveen/effervescence/Utils/OrganizersList.java
@@ -11,16 +11,27 @@ import java.util.ArrayList;
 public class OrganizersList {
     public static final ArrayList<Person> organizers= new ArrayList<>();
     static {
-        organizers.add(new Person(R.drawable.sample_profile,"Pranjal Paliwal","App Developer","Geekhaven","7206197202","http://fb.com/betterclever","http://github.com/betterclever",R.color.md_brown_900));
-        organizers.add(new Person(R.drawable.sample_profile,"Better Clever","App Developer","Geekhaven","7206197202","http://fb.com/betterclever","http://github.com/betterclever",R.color.md_green_900));
+        organizers.add(new Person(R.drawable.pranav,"Pranav Pandey ","Festival Coordinator","Geekhaven","8604241015","http://fb.com/betterclever","http://github.com/betterclever",R.color.md_brown_900));
+        organizers.add(new Person(R.drawable.sundar,"Ritwick Sundar","Festival Coordinator","Geekhaven","7080190417","http://fb.com/betterclever","http://github.com/betterclever",R.color.md_green_900));
 
-        organizers.add(new Person(R.drawable.sample_profile,"Pranjal Paliwal","App Developer","Geekhaven","7206197202","http://fb.com/betterclever","http://github.com/betterclever",R.color.md_cyan_900));
-        organizers.add(new Person(R.drawable.sample_profile,"Pranjal Paliwal","App Developer","Geekhaven","7206197202","http://fb.com/betterclever","http://github.com/betterclever",R.color.md_deep_orange_900));
+        organizers.add(new Person(R.drawable.sahu,"Utkarsh Sahu","Events Management","Geekhaven","9416383071","http://fb.com/betterclever","http://github.com/betterclever",R.color.md_cyan_900));
+        organizers.add(new Person(R.drawable.shrikant,"Shrikant Bhaskar","Sponsorship","Geekhaven","7052858328","http://fb.com/betterclever","http://github.com/betterclever",R.color.md_deep_orange_900));
 
-        organizers.add(new Person(R.drawable.sample_profile,"Pranjal Paliwal","App Developer","Geekhaven","7206197202","http://fb.com/betterclever","http://github.com/betterclever",R.color.md_deep_purple_900));
-        organizers.add(new Person(R.drawable.sample_profile,"Better Clever","App Developer","Geekhaven","7206197202","http://fb.com/betterclever","http://github.com/betterclever",R.color.md_blue_grey_900));
+        organizers.add(new Person(R.drawable.eashwer,"Eashwer Ram","Sponsorship","Geekhaven","8903473601","http://fb.com/betterclever","http://github.com/betterclever",R.color.md_deep_purple_900));
+        organizers.add(new Person(R.drawable.kshitij,"Kshitij Garg","Public Relations","Geekhaven","7052395446","http://fb.com/betterclever","http://github.com/betterclever",R.color.md_blue_grey_900));
 
-        organizers.add(new Person(R.drawable.sample_profile,"Better Clever","App Developer","Geekhaven","7206197202","http://fb.com/betterclever","http://github.com/betterclever",R.color.md_orange_800));
-        organizers.add(new Person(R.drawable.sample_profile,"Pranjal Paliwal","App Developer","Geekhaven","7206197202","http://fb.com/betterclever","http://github.com/betterclever",R.color.md_light_green_900));
+        organizers.add(new Person(R.drawable.meghana,"Meghana Pammi","Sponsorship","Geekhaven","9559155952","http://fb.com/betterclever","http://github.com/betterclever",R.color.md_deep_purple_900));
+        organizers.add(new Person(R.drawable.nishit,"Nishit Gupta","Finance","Geekhaven","8090937036","http://fb.com/betterclever","http://github.com/betterclever",R.color.md_blue_grey_900));
+
+        organizers.add(new Person(R.drawable.namit,"Namit Mishra","Logistics And Branding","Geekhaven","7570020165","http://fb.com/betterclever","http://github.com/betterclever",R.color.md_deep_purple_900));
+        organizers.add(new Person(R.drawable.soni,"Divyesh Soni","Creatives","Geekhaven","9838228283","http://fb.com/betterclever","http://github.com/betterclever",R.color.md_blue_grey_900));
+
+
+        organizers.add(new Person(R.drawable.deora,"Abhishek Deora","Web Operations","Geekhaven","7570020165","http://fb.com/betterclever","http://github.com/betterclever",R.color.md_deep_purple_900));
+        organizers.add(new Person(R.drawable.soni,"Yogesh Gupta","Web Operations","Geekhaven","9404173123","http://fb.com/betterclever","http://github.com/betterclever",R.color.md_blue_grey_900));
+
+
+        organizers.add(new Person(R.drawable.naveen,"Naveen Shukla","App Operations","Geekhaven","7376415693","http://fb.com/betterclever","http://github.com/betterclever",R.color.md_deep_purple_900));
+
     }
 }
